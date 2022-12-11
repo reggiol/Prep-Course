ea076585bdb9f6037795d831c47398be9d8dd32f
reggiol <lucasreggio@gmail.com>
2022-12-11 13:09:32 -0300

    domingo

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -213,8 +213,20 @@ function tablaDelSeis(){
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
+  if(Number.isInteger(numero)){
+    if((numero.toString()).length == 3) {
+      return true
+    }
   
+    else {
+    return false
+    }
 }
+  else {
+    return false
+  }
+}
+
 
 function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
